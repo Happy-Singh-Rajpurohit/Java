9ae9a41dac2d8341aefbadf0dbc65935b60a261a
Happy-Singh-Rajpurohit <happyrajpurohit2006@gmail.com>
2025-08-18 00:02:41 +0530

    ARRAY-2

---
+++ Array-2/SubarraySum3.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class SubarraySum3 {
+    public static void maxSubarraySum(int numbers[]){
+        int currSum =0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+        prefix[0] = numbers[0];
+        //Calculate Prefix Array
+        for(int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
+
+        
+        for(int i=0; i<numbers.length; i++){
+            int start = i;
+            for(int j=i; j<numbers.length; j++){
+                int end =j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+                
+                if(maxSum<currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max SUM: "+ maxSum);
+    }
+
+    public static void kadanes(int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<numbers.length; i++){
+            cs = cs + numbers[i];
+            if(cs<0){
+                cs =0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("Max sum is : "+ ms);
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {1,2,3,4,5,6,9};
+        kadanes(numbers);    
+    }
+}
