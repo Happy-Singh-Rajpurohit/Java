871fdbb67f793458548093f8e8092863fb2a8eec
Happy-Singh-Rajpurohit <happyrajpurohit2006@gmail.com>
2025-10-24 17:08:29 +0530

    Basic-Sorting-Algorithm

---
+++ Basic Sorting Algorithm/InBuiltSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class InBuiltSort {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        Arrays.sort(arr);
+        Arrays.sort(arr, 0, 3); //By passing starting and ending index
+        printArr(arr);
+    }
+}
